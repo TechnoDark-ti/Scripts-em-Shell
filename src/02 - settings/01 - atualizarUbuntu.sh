6c1018a027fcdb1a618085e0d581360b8c348c69
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-04-01 15:39:31 -0300

    Update 01 - atualizarUbuntu.sh

--- src/02 - settings/01 - atualizarUbuntu.sh
+++ src/02 - settings/01 - atualizarUbuntu.sh
@@ -1,7 +1,26 @@
+# @Author: Márcio Moda
+# Data: 01-04-2025
+#
+#
+## O que esse código faz? ##
+# # Ele atualiza o sistema operacional Ubuntu Server, incluindo pacotes, 
+#dependências e removendo pacotes desnecessários.
+########### FIM DOS COMENTÁROIOS ###########
+
 #!/bin/bash
+
+
 
 sudo apt update
 
 sudo apt upgrade -y
 
 sudo apt full-upgrade -y
+
+sudo apt dist-upgrade -y
+
+sudo apt autoremove -y
+
+sudo apt autoclean -y
+
+sudo apt clean
