65065b6af79d049dc4d2b077b5a4f445308f1996
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-04-11 08:06:45 -0300

    Update 01 - activeDiretoryDomain.sh

--- src/04 - AD/01 - activeDiretoryDomain.sh
+++ src/04 - AD/01 - activeDiretoryDomain.sh
@@ -19,47 +19,25 @@ sudo vim /etc/hosts
 
 sudo vim /etc/hostname
 
-sudo apt install -y autoconf bind9utils bison debhelper dnsutils \
-                    docbook-xml docbook-xsl flex gdb libjansson-dev \
-                    libacl1-dev libaio-dev libarchive-dev libattr1-dev \
-                    libblkid-dev libbsd-dev libcap-dev libcups2-dev \
-                    libgnutls28-dev libgpgme11-dev libjson-perl libldap2-dev \
-                    libncurses5-dev libpam0g-dev libparse-yapp-perl libpopt-dev\
-                    libreadline-dev nettle-dev perl perl-modules pkg-config\
-                    python-all-dev python-crypto python-dbg python-dev\
-                    python-dnspython python3-dnspython python-gpg python3-gpg\
-                    python-markdown python3-markdown python3-dev xsltproc\
-                    zlib1g-dev liblmdb-dev lmdb-utils libsystemd-dev
+sudo apt install -y realmd sssd sssd-tools adcli samba-common-bin \
+                    oddjob oddjob-mkhomedir packagekit
+
+realm discover domain.local
+sudo realm join --user=Administrador domain.local
+realm list # Validar se o domínio foi adicionado corretamente
+sudo pam-auth-update
+
+#Habilite a opção:
+#[✔] Create home directory on login
+
+sudo realm permit --all
+
 
 sudo reboot 
-
-sudo apt install --y samba krb5-user winbind \
-                    libnss-winbind smbclient \
-                    ldap-utils acl attr ntp
-
-sudo systemctl stop smbd.service &&  sudo systemctl stop nmbd.service
-
-sudo systemctl stop winbind.service
-
-cd /etc/samba
-
-sudo mv smb.conf smb.conf.bkp
-
-sudo samba-tool domain provision --use-rfc2307 --interactive
 
 # Iniciar os serviços que foram parados anteriormente:
 
 /etc/init.d/samba-ad-dc restart
-
-# Ajustar os scripts para que os serviços inicializem corretamente
-
-sudo systemctl unmask samba-ad-dc.service && sudo systemctl enable samba-ad-dc.service
-
-sudo systemctl restart samba-ad-dc.service
-
-# Verificar o status do serviço do Samba:
-
-sudo systemctl status samba-ad-dc.service
 
 # Abrir o arquivo “/etc/resolv.conf” e ajustar as configurações
 
