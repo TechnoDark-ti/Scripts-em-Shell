df498ced9cae7640169119d06f056991352f59f0
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-27 20:27:46 -0300

    Update bd_ws.sql

--- src/scan/bd_ws.sql
+++ src/scan/bd_ws.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE auditoria_rede;
-USE auditoria_rede;
+CREATE DATABASE AUDITORIA_REDE;
+USE AUDITORIA_REDE;
 
-CREATE TABLE Usuario (
+CREATE TABLE USUARIO (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     email VARCHAR(100) UNIQUE,
     senha_hash VARCHAR(255)
 );
 
-CREATE TABLE Captura (
+CREATE TABLE CAPTURA (
     id_captura INT AUTO_INCREMENT PRIMARY KEY,
     data_hora_inicio DATETIME,
     interface VARCHAR(50),
@@ -16,14 +16,14 @@ CREATE TABLE Captura (
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
-CREATE TABLE Filtro (
+CREATE TABLE FILTRO (
     id_filtro INT AUTO_INCREMENT PRIMARY KEY,
     expressao TEXT,
     id_captura INT,
     FOREIGN KEY (id_captura) REFERENCES Captura(id_captura)
 );
 
-CREATE TABLE Relatorio (
+CREATE TABLE RELATORIO (
     id_relatorio INT AUTO_INCREMENT PRIMARY KEY,
     nome_arquivo VARCHAR(100),
     data_geracao DATETIME,
