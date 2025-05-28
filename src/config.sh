c0adce07ec61a1cef88fdba2daae6f641483cdd1
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-28 11:36:31 -0300

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -1,5 +1,5 @@
 echo "deb https://www.deb-multimedia.org bookworm main non-free" > /etc/apt/sources.list.d/deb-multimedia.list
-apt-get update -oAcquire::AllowInsecureRepositories=true
-apt-get install deb-multimedia-keyring
-apt-get update; apt-get dist-upgrade
+apt update -oAcquire::AllowInsecureRepositories=true
+apt install deb-multimedia-keyring
+apt update && apt dist-upgrade
 
