14bd8b530b4f9464a18d3a2cc2a04f0a477e065d
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-28 11:36:27 -0300

    Update install_macos.sh

--- src/06 - configs-desktops/install_macos.sh
+++ src/06 - configs-desktops/install_macos.sh
@@ -1,14 +1,18 @@
 # instalar e configurar os macbooks OS
 
 #configurar o macbook
-sudo --scutil --set ComputerName PC001.infra.local # set the computer name
-sudo --scutil --set HostName PC001.infra.local # set the host name
-sudo --scutil --set LocalHostName PC001 # set the local host name
+sudo scutil --set ComputerName PC001.infra.local # set the computer name
+sudo scutil --set HostName PC001.infra.local # set the host name
+sudo scutil --set LocalHostName PC001 # set the local host name
 
 #instalar alguns aplicativos como: Chrome, LibreOffice, Transmission, Unarchiver e VLC
-curl -s 'https://api.macapps.link/pt/chrome-libreoffice-transmission-unarchiver-vlc' | sh
+#curl -s 'https://api.macapps.link/pt/chrome-libreoffice-transmission-unarchiver-vlc' | sh
 
 #instalar o brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install wine speedtest-cli wget nmap htop tree jq git python3 python3-pip 
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/user/.zprofile # trocar o user pelo seu usuario
+eval "$(/opt/homebrew/bin/brew/ shellenv)" 
+
+brew install wine speedtest-cli htop tree git python3 python3-pip openjdk@17 libreoffice anydesk google-chrome
