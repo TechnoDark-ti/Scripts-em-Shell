ca685ebe6d3e1333aaa35eab6e91908ad6e1e817
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-10 14:13:07 -0300

    Create 06 - installTigerVNC.sh

---
+++ src/03 - services/06 - installTigerVNC.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+#Serviço de acesso remoto via vnc para qualquer sistema operacional
+#
+#@Author: Márcio Moda 
+
+#instalação do serviço vnc server
+sudo apt install tigervnc-standalone-server
+
+#configurar o passwd sem o sudo
+vncpasswd
+
+#Configurar o arquivo vnc para usar o xfce
+cd ~/.vnc
+touch xstartup
+echo "#!/bin/bash \ unset SESSION_MANAGER \ unset DBUS_SESSION_MANAGER \ exec startxfce4" > xstartup
+
+#Permitir que ele seja um executável
+chmod u+x ~/.vnc/xstartup
+
+#instalar um servidor ssh (opicional)
+sudo apt install openssh-server
+
+
